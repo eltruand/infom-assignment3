bfd1625cdff9b2bd13db6e5c4afb10bdf518b684
eltruand <ziegldom@students.zhaw.ch>
2021-06-12 21:46:21 +0200

    Finale Anpassung AppRepo & AppRestC

--- InfoM.Assignment3/src/main/java/assignment3/repositories/SteuerappRepository.java
+++ InfoM.Assignment3/src/main/java/assignment3/repositories/SteuerappRepository.java
@@ -1,11 +1,16 @@
 package assignment3.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import assignment3.entities.Steuerapp;
+import assignment3.entities.User;
 
 public interface SteuerappRepository extends JpaRepository<Steuerapp, Long> {
 
-	//SELECT USER WHERE FK_STEUERERKLÄRUNG == ID_STEUERERKLÄRUNG && FK_USER == ID_USER
-	
+	@Query("SELECT u FROM User u, Steuererklärung se, Steuerapp sa WHERE u.id=sa.person AND sa.steuererklärung = se.id AND u.id = ?1 AND se.erstelldatum = ?2")
+	public List<User> getUsersBySteuererklärungByErstelldatum(long steuererklärungid, int erstelldatum);
+
 }
--- InfoM.Assignment3/src/main/java/assignment3/restcontroller/SteuerappRestController.java
+++ InfoM.Assignment3/src/main/java/assignment3/restcontroller/SteuerappRestController.java
@@ -1,10 +1,17 @@
 package assignment3.restcontroller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import assignment3.repositories.AdminRepository;
+import assignment3.entities.User;
 import assignment3.repositories.SteuerappRepository;
 import assignment3.repositories.SteuererklärungRepository;
 import assignment3.repositories.UserRepository;
@@ -22,10 +29,16 @@ public class SteuerappRestController {
 	@Autowired
 	private UserRepository userrepository;
 
-	@Autowired
-	private AdminRepository adminrepository;
-	
-	
-	@RequestMap
+	@RequestMapping(value = "assignment3/users/{steuerid}/{erstelldatum}", method = RequestMethod.GET)
+	public ResponseEntity<List<User>> getUsersBySteuererklärungByErstelldatum(
+			@PathVariable("steuererid") long steuererklärungid, @PathVariable("erstelldatum") int erstelldatum) {
+		List<User> result = this.repository.getUsersBySteuererklärungByErstelldatum(steuererklärungid, erstelldatum);
+
+		if (!result.isEmpty()) {
+			return new ResponseEntity<List<User>>(result, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<List<User>>(HttpStatus.NOT_FOUND);
+		}
+	}
 
 }
